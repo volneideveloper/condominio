f546b81120f53c2155783f40cb8a65d97e4623f7
Volnei Roberto Weiss <volneideveloper@gmail.com>
2025-09-08 07:40:36 -0300

    inclussao das rotas

--- app/Http/Controllers/CondominiumController.php
+++ app/Http/Controllers/CondominiumController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Condominium;
 use Illuminate\Http\Request;
 
-class CondominioController extends Controller
+class CondominiumController extends Controller
 {
     public function index()
     {
--- routes/web.php
+++ routes/web.php
@@ -2,22 +2,24 @@
 
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CondominioController;
+use App\Http\Controllers\CondominiumController;
+use App\Http\Controllers\CondominiumImageController;
+use App\Http\Controllers\CondominiumStatusController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/login', function () {
-    return view('auth.login');
-})->name('login');
-
-// Route::group(['middleware' => ['auth']], function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
-// });
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::middleware(['auth'])->group(function () {
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+    Route::resource('condominio',CondominiumController::class);
+    Route::resource('condominio-imagems',CondominiumImageController::class);
+    Route::resource('condominio-status',CondominiumStatusController::class);
+
+});
 
